8379655e053116130968d083f2b9e0fe04ff0cb5
Mustafa <mostafakhaled0787314@gmail.com>
2023-09-23 18:27:56 +0300

    fix a small issue

--- src/components/navbar/LogoutIcon.jsx
+++ src/components/navbar/LogoutIcon.jsx
@@ -9,12 +9,11 @@ function LogoutIcon() {
 
   return (
     <div>
-      <div onClick={() => setShowUserInfo((u) => !u)}>
+      <div onClick={() => setShowUserInfo(!showUserInfo)} ref={userInfoRef}>
         <AccountCircleIcon className="text-colorBrand" />
       </div>
       {/* User Info Box */}
       <div
-        ref={userInfoRef}
         className={`bg-colorGrey2 text-textColor shadow-md rounded-[10px] p-[10px] fixed transition-right duration-300  ease-in-out ${
           showUserInfo ? "right-[10px]" : "right-[-100%]"
         } top-[60px]`}>
