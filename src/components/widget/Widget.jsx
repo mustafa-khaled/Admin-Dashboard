55f4eacfa115dac2dab6c29458216dc98dade5ce
Mustafa <mostafakhaled0787314@gmail.com>
2024-08-04 10:07:34 +0300

    edit in wesges

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -7,7 +7,7 @@ function Widget({ widget, amount, percentage }) {
     <div className="flex flex-[1] p-[20px] justify-between  bg-colorGrey2  shadow-md rounded-[10px] min-h-[100px] ">
       <div className="flex flex-col justify-between">
         <span className="text-[14px] font-semibold">{widget.title}</span>
-        <span className="text-[28px] font-bold">{amount || widget.amount}</span>
+        <span className="text-[28px] font-bold">{amount || 0}</span>
         <span className="text-[12px] cursor-pointer underline">
           <Link to={widget.to}>{widget.link}</Link>
         </span>
